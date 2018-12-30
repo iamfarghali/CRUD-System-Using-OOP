5f140f97718ab6c5feee30e4b9d3309d1e5c3b83
Muhammed A.Farghali <muhammad.farghali14@gmail.com>
2018-12-30 08:34:50 +0200

    Added pages folder

---
+++ pages/product/add_product.php
@@ -0,0 +1,15 @@
+<?php
+// set page headers
+$pageTitle = "Add Product";
+include_once "../../layout/header.php";
+?>
+
+<div class="right-botton-margin">
+	<a href="index.php" class="btn btn-default pull-right">Read Products</a>
+</div>
+
+
+<?php
+// footer
+include_once "../../layout/footer.php";
+?>
